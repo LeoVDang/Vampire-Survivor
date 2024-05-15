39f54bc82eebca3678c0ed63e8bb63c214483d94
LeoVDang <161635420+LeoVDang@users.noreply.github.com>
2024-05-15 10:09:52 -0700

    Giving Gun to Player

--- Vampire Survivor/mob.gd
+++ Vampire Survivor/mob.gd
@@ -7,6 +7,6 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 200.0
+	velocity = direction * 250.0
 	move_and_slide()
 
